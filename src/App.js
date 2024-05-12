4f9e82b87c0f3c2326fadaad885cc49f6bb811cf
Santhosh Kumar <108540429+SanthoshKumar-SanthoshKumar@users.noreply.github.com>
2024-05-12 18:52:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+
+import Header from './components/Header';
+
+import Footer from './components/Footer';
+
+import { Helmet } from 'react-helmet';
+
 import './App.css';
+import Products from './components/Products';
+
 
 function App() {
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <article>
+      <Helmet>
+        <title>AppScrip Store</title>
+        <meta name="description" content="AppScrip Online Store for clothing and Grocerries" />
+        {/* Other head elements */}
+      </Helmet>
+      <Header/>
+      <section className='mobile-view-only'>
+        <h1>HOME</h1>
+        <p>|</p>
+        <h2>SHOP</h2>
+      </section>
+      <section className='title-container'>
+          <h1>DISCOVER OUR PRODUCTS</h1>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non voluptatibus, 
+            consequuntur maxime id dolor</p>
+      </section>
+      <Products/>
+      <footer>
+        <Footer/>
+      </footer> 
+    </article>
+  </div>
   );
 }
 
 export default App;
+
